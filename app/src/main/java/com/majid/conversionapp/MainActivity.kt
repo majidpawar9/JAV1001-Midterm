e3ebde0446ad19f29cea3952687690a7f4aeb2df
majidpawar9 <majidpawar9@gmail.com>
2023-07-08 15:43:14 -0400

    added functionality to spinner

--- app/src/main/java/com/majid/conversionapp/MainActivity.kt
+++ app/src/main/java/com/majid/conversionapp/MainActivity.kt
@@ -2,10 +2,50 @@ package com.majid.conversionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val userUnit = arrayOf("Inch","Centimeter","Meter","Kilometer","Miles","Gram","Kilogram","Pound")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinnerFrom = findViewById<Spinner>(R.id.convertSpinnerFrom)
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,userUnit)
+        spinnerFrom.adapter = arrayAdapter
+        spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext,"Selected Unit is "+userUnit[position],Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+        val spinnerTo = findViewById<Spinner>(R.id.convertSpinnerTo)
+        spinnerTo.adapter = arrayAdapter
+        spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext,"Selected Unit is "+userUnit[position],Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
+
 }
