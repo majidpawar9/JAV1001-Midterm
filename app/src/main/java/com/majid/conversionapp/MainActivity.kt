d5d0c0d5fd1f09bfbf6dfd974c94c537de63c61a
majidpawar9 <majidpawar9@gmail.com>
2023-07-08 16:34:09 -0400

    added button and toast functionality to test

--- app/src/main/java/com/majid/conversionapp/MainActivity.kt
+++ app/src/main/java/com/majid/conversionapp/MainActivity.kt
@@ -5,47 +5,81 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
-    private val userUnit = arrayOf("Inch","Centimeter","Meter","Kilometer","Miles","Gram","Kilogram","Pound")
+    private val userUnit =
+        arrayOf("Inch", "Centimeter", "Meter", "Kilometer", "Miles", "Gram", "Kilogram", "Pound")
+    private lateinit var spinnerFrom:Spinner
+    private lateinit var spinnerTo:Spinner
+    private lateinit var convertButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val spinnerFrom = findViewById<Spinner>(R.id.convertSpinnerFrom)
-        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,userUnit)
+
+        val arrayAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, userUnit)
+
+        spinnerFrom = findViewById<Spinner>(R.id.convertSpinnerFrom)
         spinnerFrom.adapter = arrayAdapter
-        spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                Toast.makeText(applicationContext,"Selected Unit is "+userUnit[position],Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Selected Unit is " + userUnit[position],
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                Toast.makeText(applicationContext,"Nothing Selected! Please select a unit",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Nothing Selected! Please select a unit",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
-        val spinnerTo = findViewById<Spinner>(R.id.convertSpinnerTo)
+
+        spinnerTo = findViewById<Spinner>(R.id.convertSpinnerTo)
         spinnerTo.adapter = arrayAdapter
-        spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                Toast.makeText(applicationContext,"Selected Unit is "+userUnit[position],Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Selected Unit is " + userUnit[position],
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                Toast.makeText(applicationContext,"Nothing Selected! Please select a unit",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Nothing Selected! Please select a unit",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+        }
+        convertButton = findViewById(R.id.convertButton)
+        convertButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Unit is " + spinnerTo.selectedItem.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
-
 }
