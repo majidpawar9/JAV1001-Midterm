0f84b8ef277964443c97e6408e30fe16e7f239dc
majidpawar9 <majidpawar9@gmail.com>
2023-07-08 20:48:46 -0400

    added strings to string.xml and added comments

--- app/src/main/java/com/majid/conversionapp/Conversion.kt
+++ app/src/main/java/com/majid/conversionapp/Conversion.kt
@@ -1,78 +1,83 @@
 package com.majid.conversionapp
-fun main(){
-    val con = Conversion()
-    println(con.conversionKit("123444", "Milimeter","Centimeter"))
-}
 
 class Conversion {
-    fun conversionKit(userUnit: CharSequence, convertFrom:String, convertTo:String):String{
-        val userUnitString = userUnit.toString()
-        val userUnitInt = userUnitString.toDouble()
+    // ConversionKit function which accepts userUnit
+    fun conversionKit(userValue: CharSequence, convertFrom:String, convertTo:String):String{
+        val userValueString = userValue.toString()
+        val userValueDouble = userValueString.toDouble()
+
+        //If null value is passed
+        if(userValueDouble==null){
+            return "Please Enter a Value!"
+        }
         var convertedValue = 0.0
         var result = ""
-        if (userUnitInt < 0) {
+
+        //if negative value is passed
+        if (userValueDouble < 0) {
             result = "Please Enter a Positive Value"
         }
+        // Conversion Kit to convert from every unit
         else{
             when (convertFrom){
                 "Milimeter" -> {
                     when(convertTo){
-                        "Centimeter" -> {convertedValue = userUnitInt * 0.1}
-                        "Inch" -> {convertedValue = userUnitInt * 0.0393701}
-                        "Meter" -> {convertedValue = userUnitInt * 0.001}
-                        "Kilometer" -> {convertedValue = userUnitInt * 0.000001}
-                        "Miles" -> {convertedValue = userUnitInt * 0.000000621371}
-                        "Milimeter" -> {convertedValue = userUnitInt * 1}
+                        "Centimeter" -> {convertedValue = userValueDouble * 0.1}
+                        "Inch" -> {convertedValue = userValueDouble * 0.0393701}
+                        "Meter" -> {convertedValue = userValueDouble * 0.001}
+                        "Kilometer" -> {convertedValue = userValueDouble * 0.000001}
+                        "Miles" -> {convertedValue = userValueDouble * 0.000000621371}
+                        "Milimeter" -> {convertedValue = userValueDouble * 1}
                     }
                 }
                 "Centimeter" -> {
                     when(convertTo){
-                        "Milimeter" -> {convertedValue = userUnitInt * 10}
-                        "Inch" -> {convertedValue = userUnitInt * 0.393701}
-                        "Meter" -> {convertedValue = userUnitInt * 0.01}
-                        "Kilometer" -> {convertedValue = userUnitInt * 0.00001}
-                        "Miles" -> {convertedValue = userUnitInt * 0.00000621371}
-                        "Centimeter" -> {convertedValue = userUnitInt * 1}
+                        "Milimeter" -> {convertedValue = userValueDouble * 10}
+                        "Inch" -> {convertedValue = userValueDouble * 0.393701}
+                        "Meter" -> {convertedValue = userValueDouble * 0.01}
+                        "Kilometer" -> {convertedValue = userValueDouble * 0.00001}
+                        "Miles" -> {convertedValue = userValueDouble * 0.00000621371}
+                        "Centimeter" -> {convertedValue = userValueDouble * 1}
                     }
                 }
                 "Inch" -> {
                     when(convertTo){
-                        "Milimeter" -> {convertedValue = userUnitInt * 25.4}
-                        "Centimeter" -> {convertedValue = userUnitInt * 2.54}
-                        "Meter" -> {convertedValue = userUnitInt * 0.0254}
-                        "Kilometer" -> {convertedValue = userUnitInt * 0.0000254}
-                        "Miles" -> {convertedValue = userUnitInt * 0.00000157828}
-                        "Inch" -> {convertedValue = userUnitInt * 1}
+                        "Milimeter" -> {convertedValue = userValueDouble * 25.4}
+                        "Centimeter" -> {convertedValue = userValueDouble * 2.54}
+                        "Meter" -> {convertedValue = userValueDouble * 0.0254}
+                        "Kilometer" -> {convertedValue = userValueDouble * 0.0000254}
+                        "Miles" -> {convertedValue = userValueDouble * 0.00000157828}
+                        "Inch" -> {convertedValue = userValueDouble * 1}
                     }
                 }
                 "Meter" -> {
                     when(convertTo){
-                        "Milimeter" -> {convertedValue = userUnitInt * 1000}
-                        "Centimeter" -> {convertedValue = userUnitInt * 100}
-                        "Inch" -> {convertedValue = userUnitInt * 39.3701}
-                        "Kilometer" -> {convertedValue = userUnitInt * 0.001}
-                        "Miles" -> {convertedValue = userUnitInt * 0.000621371}
-                        "Meter" -> {convertedValue = userUnitInt * 1}
+                        "Milimeter" -> {convertedValue = userValueDouble * 1000}
+                        "Centimeter" -> {convertedValue = userValueDouble * 100}
+                        "Inch" -> {convertedValue = userValueDouble * 39.3701}
+                        "Kilometer" -> {convertedValue = userValueDouble * 0.001}
+                        "Miles" -> {convertedValue = userValueDouble * 0.000621371}
+                        "Meter" -> {convertedValue = userValueDouble * 1}
                     }
                 }
                 "Kilometer" -> {
                     when(convertTo){
-                        "Milimeter" -> {convertedValue = userUnitInt * 1000000}
-                        "Centimeter" -> {convertedValue = userUnitInt * 100000}
-                        "Inch" -> {convertedValue = userUnitInt * 39370.1}
-                        "Meter" -> {convertedValue = userUnitInt * 1000}
-                        "Miles" -> {convertedValue = userUnitInt * 0.621371}
-                        "Kilometer" -> {convertedValue = userUnitInt * 1}
+                        "Milimeter" -> {convertedValue = userValueDouble * 1000000}
+                        "Centimeter" -> {convertedValue = userValueDouble * 100000}
+                        "Inch" -> {convertedValue = userValueDouble * 39370.1}
+                        "Meter" -> {convertedValue = userValueDouble * 1000}
+                        "Miles" -> {convertedValue = userValueDouble * 0.621371}
+                        "Kilometer" -> {convertedValue = userValueDouble * 1}
                     }
                 }
                 "Miles" -> {
                     when(convertTo){
-                        "Milimeter" -> {convertedValue = userUnitInt * 1609344}
-                        "Centimeter" -> {convertedValue = userUnitInt * 160934}
-                        "Inch" -> {convertedValue = userUnitInt * 63360}
-                        "Meter" -> {convertedValue = userUnitInt * 1609.34}
-                        "Kilometer" -> {convertedValue = userUnitInt * 1.60934}
-                        "Miles" -> {convertedValue = userUnitInt * 1}
+                        "Milimeter" -> {convertedValue = userValueDouble * 1609344}
+                        "Centimeter" -> {convertedValue = userValueDouble * 160934}
+                        "Inch" -> {convertedValue = userValueDouble * 63360}
+                        "Meter" -> {convertedValue = userValueDouble * 1609.34}
+                        "Kilometer" -> {convertedValue = userValueDouble * 1.60934}
+                        "Miles" -> {convertedValue = userValueDouble * 1}
                     }
                 }
             }
--- app/src/main/java/com/majid/conversionapp/MainActivity.kt
+++ app/src/main/java/com/majid/conversionapp/MainActivity.kt
@@ -7,16 +7,27 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.get
+import com.majid.conversionapp.databinding.ActivityMainBinding
 
+////////////////////////
+// SpinnerFrom is used to define the unit it is being converted FROM
+// SpinnerTo is used to define the unit it is being converted TO
+////////////////////////
 class MainActivity : AppCompatActivity() {
+    //Units array
     private val userUnit =
-        arrayOf("Inch", "Centimeter", "Meter", "Kilometer", "Miles", "Gram", "Kilogram", "Pound")
+        arrayOf("Milimeter", "Centimeter", "Inch", "Meter", "Kilometer", "Miles")
+
+    //used lateinit to keep to property from being initialized
     private lateinit var spinnerFrom:Spinner
     private lateinit var spinnerTo:Spinner
     private lateinit var convertButton: Button
+    private lateinit var answerText: TextView
+    private lateinit var binding: ActivityMainBinding
 
+    //Main function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +35,11 @@ class MainActivity : AppCompatActivity() {
         val arrayAdapter =
             ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, userUnit)
 
+
+        var convertFromString:String = ""
+        var convertToString:String = ""
+
+        // Defining the spinner dropdown using arrayAdapter and assigning it the values of userUnit Array
         spinnerFrom = findViewById<Spinner>(R.id.convertSpinnerFrom)
         spinnerFrom.adapter = arrayAdapter
         spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -33,11 +49,13 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
+                // When user selects a unit a toast message will be shown
                 Toast.makeText(
                     applicationContext,
                     "Selected Unit is " + userUnit[position],
                     Toast.LENGTH_SHORT
                 ).show()
+                convertFromString = userUnit[position]
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -49,6 +67,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Defining the spinner dropdown using arrayAdapter and assigning it the values of userUnit Array
         spinnerTo = findViewById<Spinner>(R.id.convertSpinnerTo)
         spinnerTo.adapter = arrayAdapter
         spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -58,11 +77,13 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
+                // When user selects a unit a toast message will be shown
                 Toast.makeText(
                     applicationContext,
                     "Selected Unit is " + userUnit[position],
                     Toast.LENGTH_SHORT
                 ).show()
+                convertToString = userUnit[position]
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -73,13 +94,24 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
         }
+
+        // Getting the userInput and converting it to array of Characters
+        var userInput = findViewById<TextView>(R.id.userInput)
+        var userValue = userInput.getText()
+
+        // Object of Conversion Class
+        val con = Conversion()
+
+        //
+        answerText = findViewById(R.id.outputText)
         convertButton = findViewById(R.id.convertButton)
+
         convertButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                "Unit is " + spinnerTo.selectedItem.toString(),
-                Toast.LENGTH_SHORT
-            ).show()
+            //Calling function of conversion class
+            var result = con.conversionKit(userValue,convertFromString,convertToString)
+
+            // Setting the result from conversionKit function to answerText
+            answerText.setText(result)
         }
     }
 }
